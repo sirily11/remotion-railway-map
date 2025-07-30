933ef03a4597d12adbe3632b5e7b0342cb31381a
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-07-31 00:12:43 +0800

    chore: update ESLint configuration to disable specific TypeScript rules for improved flexibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,13 @@ const eslintConfig = [
     },
     files: ["src/remotion/**"],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
